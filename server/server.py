6d13d959273f44c56a51c304ffd0d0e3d8e2f264
DNKonanov <konanovdmitriy@gmail.com>
2019-08-30 14:48:51 +0300

    next

--- server/server.py
+++ server/server.py
@@ -12,10 +12,39 @@ def answer():
 
 @app.route('/posts/')
 def posts():
-    return render_template('pages/posts.html')
+
+    #will be loaded from DB
+    posts = [
+        {
+            'id': 0,
+            'title': 'First',
+            'short_desc': 'Short desc of the First',
+            'author': 'admin',
+            'date': '01.01.2020'
+        },
+        {
+            'id': 1,
+            'title': 'Second',
+            'short_desc': 'Short desc of the Second',
+            'author': 'user',
+            'date': '06.06.2020'
+        }
+    ]
+
+    return render_template('pages/posts.html', posts=posts)
 
 @app.route('/about/')
 def about():
     return render_template('pages/about.html')
+
+@app.route('/posts/<int:id>')
+def show_post(id=0):
+
+    #will be loaded from DB by id
+    article_header = 'Phenylalanine'
+    article_text = 'Phenylalanine is an essential aromatic amino acid in humans (provided by food), Phenylalanine plays a key role in the biosynthesis of other amino acids and is important in the structure and function of many proteins and enzymes.'
+    return render_template('pages/post.html', article_text=article_text, article_header=article_header)
+
+
 
 app.run()
